6dea306c560203afe121577c5d93310be9fa34de
omar <OmarYoussry2001>
2025-06-09 13:42:05 +0300

    Add project files.

---
+++ WatchSystem/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WatchSystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
